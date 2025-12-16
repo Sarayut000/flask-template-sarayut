ea3eb1c7f54b65f84825b98c90c5db1128f0fb8c
Sarayut000 <std.67122420311@ubru.ac.th>
2025-12-16 11:27:57 +0700

    rename folder template to templates

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     title = 'Home Page'
-    return render_template('index.html', title=title)  
+    return render_template('index.html', title=title)
 
 
 @app.route('/about')
@@ -18,14 +18,14 @@ def about():
     return render_template('about.html', title=title, name=name, email=email, mobile=mobile,age=age)
 
 
-@app.route('/favorite/Foods')
+@app.route('/favorite/foods')
 def favorite_foods():
     title= 'Favorite Foods Page'
     foods = ['ส้มตำไก่ย่าง', 'สามชั้นทอดน้ำปลา', 'กระเพราหมูกรอบไข่ดาว']
     return render_template('favorite_foods.html', title=title, foods=foods)
 
 
-@app.route('/favorite/Sports')
+@app.route('/favorite/sports')
 def favorite_sports():
     title= 'Favorite Sprots Page'
     sports = ['ฟุตบอล', 'บาสเกตบอล', 'แบดมินตัน']
