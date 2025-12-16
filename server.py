1e3ab4167f26faece280c7068b88d621b18d6d95
Sarayut000 <std.67122420311@ubru.ac.th>
2025-12-16 11:05:21 +0700

    first commit

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     title = 'Home Page'
-    return render_template('index.html' , title=title)  
+    return render_template('index.html', title=title)  
 
 
 @app.route('/about')
